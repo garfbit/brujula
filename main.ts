6c4baa2234727b790e9382055f62dd69e90cb591
garfbit <104676660+garfbit@users.noreply.github.com>
2025-08-29 00:05:24 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+basic.forever(function () {
+    if (input.compassHeading() < 20 || input.compassHeading() > 340) {
+        basic.showArrow(ArrowNames.North)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+})
